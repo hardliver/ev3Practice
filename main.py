bce59f6294284b321f64b21b73825764d521f099
Hardliver <yelionel26@gmail.com>
2019-05-14 00:53:43 +0800

    Expanding the example program

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env pybricks-micropython
 
 from pybricks import ev3brick as brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import (Port, Stop, Direction, Button, Color,
-                                 SoundFile, ImageFile, Align)
-from pybricks.tools import print, wait, StopWatch
-from pybricks.robotics import DriveBase
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
 
-# Write your program here
+# Play a sound.
 brick.sound.beep()
+
+# Initialize a motor at port B.
+test_motor = Motor(Port.B)
+
+# Run the motor up to 500 degrees per second. To a target angle of 90 degrees.
+test_motor.run_target(500, 90)
+
+# Play another beep sound.
+# This time with a higher pitch (1000 Hz) and longer duration (500 ms).
+brick.sound.beep(1000, 500)
